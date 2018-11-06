3b27b373b4d0d218fea64f885331449dc055758a
Bryn Bennett <bryn.t.bennett@gmail.com>
2018-11-07 11:49:33 +1300

    Cloud function: creat admin

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,31 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-  exports.createProfile = functions.auth.user()
-  .onCreate( (userRecord, context) => {
-  return admin.database().ref(`/userProfile/${userRecord.uid}`).set({
-    email: userRecord.email,
-    name: userRecord.displayName,
-    role: "user"
+exports.createProfile = functions.auth.user()
+  .onCreate((userRecord, context) => {
+    return admin.database().ref(`/userProfile/${userRecord.uid}`).set({
+      email: userRecord.email,
+      name: userRecord.displayName,
+      role: "user"
+    });
   });
+
+exports.adminProfile = functions.database.ref('/userProfile/{profile}/role').onUpdate((event) => {
+  const user = event.data.val(); // The Firebase user.
+  const uid = event.key;
+  let customClaims = { admin: false }
+  if (user.role === 'admin') {
+    customClaims = { admin: true }
+  }
+  return admin.auth().setCustomUserClaims(uid, customClaims)
+    .then(() => {
+      // Update real-time database to notify client to force refresh.
+      const metadataRef = admin.database().ref("metadata/" + uid);
+      // Set the refresh time to the current UTC timestamp.
+      // This will be captured on the client to force a token refresh.
+      return metadataRef.set({ refreshTime: new Date().getTime() });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
