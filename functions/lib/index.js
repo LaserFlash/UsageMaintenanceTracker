c44885d012eb63a5cb46ce2b2a8d75831c7778d6
Bryn Bennett <bryn.t.bennett@gmail.com>
2018-11-07 15:25:21 +1300

    Properly format

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -11,23 +11,4 @@ exports.createProfile = functions.auth.user()
         role: "user"
     });
 });
-exports.adminProfile = functions.database.ref('/userProfile/{profile}/role').onUpdate((event) => {
-    const user = event.data.val(); // The Firebase user.
-    const uid = event.key;
-    let customClaims = { admin: false };
-    if (user.role === 'admin') {
-        customClaims = { admin: true };
-    }
-    return admin.auth().setCustomUserClaims(uid, customClaims)
-        .then(() => {
-        // Update real-time database to notify client to force refresh.
-        const metadataRef = admin.database().ref("metadata/" + uid);
-        // Set the refresh time to the current UTC timestamp.
-        // This will be captured on the client to force a token refresh.
-        return metadataRef.set({ refreshTime: new Date().getTime() });
-    })
-        .catch(error => {
-        console.log(error);
-    });
-});
 //# sourceMappingURL=index.js.map
